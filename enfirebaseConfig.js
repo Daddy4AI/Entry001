0a81500a25adcb1232f3784604fa568d2b8a86b2
Daddy4AI <alanqwyuan@gmail.com>
2024-11-23 00:26:06 +0800

    Update enfirebaseConfig.js

--- enfirebaseConfig.js
+++ enfirebaseConfig.js
@@ -3,13 +3,12 @@ import { getFirestore, initializeFirestore, persistentLocalCache } from 'https:/
 
 // Firebase 配置
 export const firebaseConfig = {
-    apiKey: "AIzaSyD4luqKnpoQvBGB4f6BuvwUAzTXqXQrnpI",
-    authDomain: "superstarw-3ddc6.firebaseapp.com",
-    projectId: "superstarw-3ddc6",
-    storageBucket: "superstarw-3ddc6.appspot.com",
-    messagingSenderId: "11393876466",
-    appId: "1:11393876466:web:667884c8ea24c95794ab6b",
-    measurementId: "G-HFFB83KWCP"
+    apiKey: "AIzaSyAE5OL3jOqHrXz4VRlN-Im3sDyiMJq2PG0",
+    authDomain: "entry001.firebaseapp.com",
+    projectId: "entry001",
+    storageBucket: "entry001.firebasestorage.app",
+    messagingSenderId: "39651467700",
+    appId: "1:39651467700:web:05392de9285dfcf72d12e1"
 };
 
 // 初始化 Firebase
